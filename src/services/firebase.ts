58c20d603cbde1eb4e8c749f94b51614e17b0d16
Pestana Catumbela <pestannapedrocatumbella@gmail.com>
2026-04-20 17:27:09 -0300

    setting up react native firebase app

---
+++ src/services/firebase.ts
@@ -0,0 +1,7 @@
+import firebase_app from '@react-native-firebase/app';
+import firebase_auth from '@react-native-firebase/auth';
+import firebase_firestore from '@react-native-firebase/firestore';
+
+export const firebaseApp = firebase_app.app();
+export const firebaseAuth = firebase_auth.auth();
+export const firebaseFirestore = firebase_firestore.firestore();
